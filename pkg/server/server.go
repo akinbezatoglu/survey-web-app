1b51736e6079bce68468e1c784bec17c1f1d4efa
akinbezatoglu <akn.bezatoglu@gmail.com>
2021-08-31 08:54:19 +0300

    make it better

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -74,7 +74,7 @@ func (i *Instance) Start(file string) {
 		Handler: router,
 	}
 
-	err = i.httpServer.ListenAndServe()
+	err = i.httpServer.ListenAndServeTLS("cert.pem", "key.pem")
 	if err != http.ErrServerClosed {
 		logrus.WithError(err).Error("HTTP Server stopped unexpected")
 		i.Shutdown()
